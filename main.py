3a0251d20b60bde00d042840153c59c64c959a62
samahrens <89820822+samahrens@users.noreply.github.com>
2022-05-25 23:37:46 -0700

    created print_board

--- main.py
+++ main.py
@@ -9,3 +9,17 @@ board = [
     [1,2,0,0,0,7,4,0,0],
     [0,4,9,2,0,6,0,0,7]
 ]
+
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
