6a3db97d7d10580f6ce1e36b8010cd03298880f7
samahrens <89820822+samahrens@users.noreply.github.com>
2022-05-30 12:27:38 -0700

    created find_empty

--- main.py
+++ main.py
@@ -23,3 +23,11 @@ def print_board(bo):
                 print(bo[i][j])
             else:
                 print(str(bo[i][j]) + " ", end="")
+
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return (i, j)  # row, col
+
+    return None
